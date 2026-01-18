18bf92b0214f2d748c4233a156763e5d67bd7b50
Pawan Patil <155644304+pavanpatil06459@users.noreply.github.com>
2026-01-18 12:25:03 +0000

    conversion of data Types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "pawan"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "pawan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "pawan" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
