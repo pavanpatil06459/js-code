f00d7f382293502d559b0a4edfb91b9086a9670f
Pawan Patil <155644304+pavanpatil06459@users.noreply.github.com>
2026-01-18 07:21:46 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JScCode as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3
+    +3) // code readability should be high
+
+console.log("pawan")
+
+let name = "pawan"
+let age = 18
+let isLoggedIn = false
+
+
+//primitive data types 
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not asign a value 
+// symbol => unique 
+
+
+//object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
