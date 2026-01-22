0f63b2e3ee6e18a8ab0c2d52dec198db0cc88d35
Pawan Patil <155644304+pavanpatil06459@users.noreply.github.com>
2026-01-22 20:18:48 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,10 +1,14 @@
+// var c = 300
+let a = 300
 
 if (true) {
     let a = 100
-    const b = 200
-    var c = 300
+    const b = 200            // This is block scoped
+    console.log("Inner: ", a);
+    
 }   
 
-// console.log(a);
+
+console.log(a);   // and this is global scoped
 // console.log(b);
-console.log(c);
+// console.log(c);
