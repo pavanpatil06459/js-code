f2f95f7abc88de2401bcd3c8c10f3127b585c6a1
Pawan Patil <155644304+pavanpatil06459@users.noreply.github.com>
2026-01-31 20:18:21 +0000

    create if statement folder

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,15 @@
+// if statement 
+
+const isUserloggedIn = true
+
+if (isUserloggedIn) {
+    
+}
+
+<,   less than
+>,   greater than (big value)
+<=,  ex 2<=2 choti ya barabr honi chahiye // gives true result on dual conditions
+>=,  
+==,  checking equal value
+!=,  not equal to  ex: => 3 != 2
+===, check type // strict checking 
