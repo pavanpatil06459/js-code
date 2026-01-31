95802957166774d40dc12934a56514b760ec3712
Pawan Patil <155644304+pavanpatil06459@users.noreply.github.com>
2026-01-31 20:18:41 +0000

    comments

--- 04_control_flow/01_one.js
+++ 04_control_flow/01_one.js
@@ -6,10 +6,10 @@ if (isUserloggedIn) {
     
 }
 
-<,   less than
->,   greater than (big value)
-<=,  ex 2<=2 choti ya barabr honi chahiye // gives true result on dual conditions
->=,  
-==,  checking equal value
-!=,  not equal to  ex: => 3 != 2
-===, check type // strict checking 
+// <,   less than
+// >,   greater than (big value)
+// <=,  ex 2<=2 choti ya barabr honi chahiye // gives true result on dual conditions
+// >=,  
+// ==,  checking equal value
+// !=,  not equal to  ex: => 3 != 2
+// ===, check type // strict checking 
