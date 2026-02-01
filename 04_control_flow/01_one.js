a8503c7137b664330e2fcf5d001a6461bf2b64f7
Pawan Patil <155644304+pavanpatil06459@users.noreply.github.com>
2026-02-01 19:11:20 +0000

    if , if else cover in this

--- 04_control_flow/01_one.js
+++ 04_control_flow/01_one.js
@@ -12,4 +12,56 @@ if (isUserloggedIn) {
 // >=,  
 // ==,  checking equal value
 // !=,  not equal to  ex: => 3 != 2
-// ===, check type // strict checking 
+// ===, check type // strict checking
+
+
+
+// const score = 200
+
+// if (score > 100 ) {
+//     let power = "fly"
+//     console.log(`User Power: ${power}`); // this is a blocked scope 
+    
+// }
+// console.log(`User Power: ${power}`);
+
+
+
+//this is called implicit scope 
+// const balance = 1000
+
+// if (balance > 500) console.log("test"); //write like this 
+// if (balance > 500) console.log("test"), console.log("test2"); //do not write like this 
+
+// if (balance < 500) {
+//     console.log("less than");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+    
+// } 
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = false
+
+if (userLoggedIn && debitCard && 2 == 2) {
+    console.log("allow to buy courses");
+} 
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("user logged in");
+    
+}
+
+if (!loggedInFromGoogle && !loggedInFromEmail) {
+    console.log("User is not logged in because they don't have email or Google login");
+}
