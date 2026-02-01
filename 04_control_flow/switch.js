dcd7255421c6049fa20e2d28700560fa7ca31117
Pawan Patil <155644304+pavanpatil06459@users.noreply.github.com>
2026-02-01 19:26:14 +0000

    switch case is done

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,65 @@
+// switch (key) {
+//     case value:
+
+//         break;    // this is a syntax of switch
+
+//     default:
+//         break;
+// }
+
+//Number
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("january");
+
+//         break;
+//     case 2:
+//         console.log("Fab");
+
+//         break;
+//     case 3:
+//         console.log("March");
+
+//         break;
+//     case 4:
+//         console.log("april");
+
+//         break;
+
+//     default:
+//         console.log("Default case march");
+
+//         break;
+// }
+
+
+
+// String 
+const months = "feb"
+
+switch (months) {
+    case "jan":
+        console.log("january");
+
+        break;
+    case "feb":
+        console.log("Feb");
+
+        break;
+    case "march":
+        console.log("march");
+
+        break;
+    case "april":
+        console.log("april");
+
+        break;
+
+    default:
+        console.log("Default case march");
+
+        break;
+}
