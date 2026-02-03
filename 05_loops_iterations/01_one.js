e0b7cca287b41d9e3a4cd6cd68e858cf1b822880
Pawan Patil <155644304+pavanpatil06459@users.noreply.github.com>
2026-02-03 20:23:38 +0000

    for loop completed

--- 05_loops_iterations/01_one.js
+++ 05_loops_iterations/01_one.js
@@ -30,4 +30,23 @@ for (let index = 0; index < myArray.length; index++) {
 }
 
 
-// 
+// break and continue 
+
+// for (let index = 0; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         break
+//     }
+//     console.log(`value of i is ${index}`);
+    
+// }
+
+
+for (let index = 0; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue
+    }
+    console.log(`value of i is ${index}`);
+    
+}
