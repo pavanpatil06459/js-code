82ad71875ff6052163699f2c23cff6c487317f50
Pawan Patil <155644304+pavanpatil06459@users.noreply.github.com>
2026-02-04 15:24:14 +0000

    for each

---
+++ 05_loops_iterations/five.js
@@ -0,0 +1,43 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach(function (item) {
+//     console.log(item);
+    
+// } )
+
+// coding.forEach(  (item) => {
+//     console.log(item);
+    
+// } )
+
+// function prinMe(item){
+//     console.log(item);
+    
+// }
+// coding.forEach(prinMe)
+
+
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr);
+    
+// })
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "java"
+    },
+     {
+        languageName: "java",
+        languageFileName: "java"
+    },
+     {
+        languageName: "cpp",
+        languageFileName: "java"
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+    
+})
