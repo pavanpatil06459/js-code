3c015c65fc2536a8d9009e2daae4387185e7b483
Pawan Patil <155644304+pavanpatil06459@users.noreply.github.com>
2026-02-04 14:53:26 +0000

    impliment for in loop in map

--- 05_loops_iterations/four.js
+++ 05_loops_iterations/four.js
@@ -6,5 +6,23 @@ const myObject = {
 }
 
 for (const key in myObject) {
-   console.log(`${key} shortcut is for ${myObject[key]}`);
+//    console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+//   console.log(programming[key]);
+}
+
+
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")     // this is not itterable
+
+for (const key in map) {
+    // console.log(key);
 }
