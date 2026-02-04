44ed02ce8ca91b14847cfbc2a79b07de4b6fb5b9
Pawan Patil <155644304+pavanpatil06459@users.noreply.github.com>
2026-02-04 14:37:55 +0000

    create folder "four"

---
+++ 05_loops_iterations/four.js
@@ -0,0 +1,10 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+   console.log(`${key} shortcut is for ${myObject[key]}`);
+}
