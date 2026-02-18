3916f7050f2e6c228f8dd65f553153358cc99d48
Pawan Patil <155644304+pavanpatil06459@users.noreply.github.com>
2026-02-18 18:53:57 +0000

    switch case for colors

--- 07_Project1/code.js
+++ 07_Project1/code.js
@@ -1,8 +1,28 @@
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('.button')
 const body = document.querySelector("body")
 
-buttons.forEach(function(e){
-  console.log(buttons);
 
-  
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    switch (e.target.id) {
+      case 'grey':
+        document.body.style.backgroundColor = 'grey';
+        break;
+      case 'white':
+        document.body.style.backgroundColor = 'white';
+        break;
+      case 'blue':
+        document.body.style.backgroundColor = 'blue';
+        break;
+      case 'yellow':
+        document.body.style.backgroundColor = 'yellow';
+        break;
+      case 'purple':
+        document.body.style.backgroundColor = 'purple';
+        break;
+    }
+  })
 });
